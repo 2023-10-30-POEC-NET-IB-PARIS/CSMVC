f8d9d8f654287f0bf76f3854966f9e2f1996bf22
Rayan Mechety <mechety.rayan@gmail.com>
2023-12-19 11:18:32 +0100

    Ajout de la correction du premier TP

---
+++ TP/TP1 : Library/WebApplication2/Controllers/LibraryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            return View();
+        }
+        public IActionResult Lotr()
+        {
+            return View();
+        }
+    }
+}
---
+++ TP/TP1 : Library/WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Coucou()
+        {
+            return View();
+        }
+    }
+}
